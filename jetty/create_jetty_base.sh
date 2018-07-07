b0011df330cbfe3fe1c0ad79655461b93b1bfedd
Bian Jiaping <ssbianjp@gmail.com>
2018-07-07 16:31:03 +0800

    Update jetty

---
+++ jetty/create_jetty_base.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+JETTY_HOME=/opt/jetty
+JETTY_BASE=/opt/jetty-base
+
+sudo mkdir -p $JETTY_BASE
+sudo chown -R $USER: $JETTY_BASE
+cd $JETTY_BASE
+
+# IntelliJ IDEA does not support start.d
+# java -jar $JETTY_HOME/start.jar --create-startd
+java -jar $JETTY_HOME/start.jar --add-to-start=http,deploy,jmx
