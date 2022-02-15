0a219a0135b40f92d99e2d0aef356919f42e32f7
Bian Jiaping <ssbianjp@gmail.com>
2022-02-15 13:29:24 +0800

    Update iwhalecloud-vpn-routes

--- iwhalecloud-vpn-routes/ip-up.sh
+++ iwhalecloud-vpn-routes/ip-up.sh
@@ -1,22 +1,27 @@
-#!/usr/bin/bash
+#!/bin/bash
+# 连接浩鲸科技 VPN 后自动设置路由
 
 sleep 3
 
+# 找出 VPN 客户端 IP
 gateway=$(ip route list | grep ' dev ppp' | cut -d' ' -f1)
-
+# 不是浩鲸科技 VPN 时不处理
 if [[ "$gateway" != 10.* && "$gateway" != 172.* ]]; then
   exit
 fi
 
+# 浩鲸科技内网 IP 段
 subnets=(
-  '10.45.0.0/16'
+  '10.10.0.0/16'
   '10.20.0.0/16'
+  '10.45.0.0/16'
   '172.16.0.0/16'
   '172.18.0.0/16'
   '172.20.0.0/16'
   '172.21.0.0/16'
 )
 
+# 配置内网 IP 段走 VPN
 for subnet in "${subnets[@]}"; do
   ip route add $subnet via $gateway
 done
